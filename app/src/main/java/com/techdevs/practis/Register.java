11f4315c8e12d7cbaa747b9ee03bc4c52874b2b5
Alda Iulia <aldaiulia99@gmail.com>
2021-03-24 19:27:00 +0200

    Created Registration view

---
+++ app/src/main/java/com/techdevs/practis/Register.java
@@ -0,0 +1,15 @@
+package com.techdevs.practis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
