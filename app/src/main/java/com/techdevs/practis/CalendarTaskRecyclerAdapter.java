29899c2d2872ac82459f8eb78a07828ac7853813
Alda Iulia <aldaiulia99@gmail.com>
2021-05-08 09:22:05 +0300

    linked checkboxes with firestore

--- app/src/main/java/com/techdevs/practis/CalendarTaskRecyclerAdapter.java
+++ app/src/main/java/com/techdevs/practis/CalendarTaskRecyclerAdapter.java
@@ -1,19 +1,23 @@
 package com.techdevs.practis;
 
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.lang.ref.Reference;
 import java.util.List;
 
 public class CalendarTaskRecyclerAdapter extends RecyclerView.Adapter<CalendarTaskRecyclerAdapter.CalendarTaskViewHolder> {
     private List<Task> tasks;
-
     public CalendarTaskRecyclerAdapter(List<Task> tasks) {
         this.tasks = tasks;
     }
@@ -27,8 +31,21 @@ public class CalendarTaskRecyclerAdapter extends RecyclerView.Adapter<CalendarTa
     @Override
     public void onBindViewHolder(@NonNull CalendarTaskViewHolder viewHolder, int position) {
         viewHolder.setTaskName(tasks.get(position).getName());
+        viewHolder.mTask.setOnCheckedChangeListener(null);
+        viewHolder.mTask.setChecked(tasks.get(position).isDone());
+        viewHolder.mTask.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                tasks.get(position).setDone(isChecked);
+                FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+                firebaseFirestore.collection("tasks").document(String.valueOf(tasks.get(position).getTaskID())).update("done", isChecked);
+            }
+        });
+
         if(tasks.get(position).isUrgent())
             viewHolder.setTaskUrgent();
+
+
     }
 
     @Override
@@ -39,7 +56,7 @@ public class CalendarTaskRecyclerAdapter extends RecyclerView.Adapter<CalendarTa
     public class CalendarTaskViewHolder extends RecyclerView.ViewHolder{
 
         private View mView;
-        private CheckBox mTask;
+        public CheckBox mTask;
         ImageView urgent;
         public CalendarTaskViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -49,10 +66,13 @@ public class CalendarTaskRecyclerAdapter extends RecyclerView.Adapter<CalendarTa
         public void setTaskName(String title){
             mTask = mView.findViewById(R.id.task);
             mTask.setText(title);
+
         }
+
         public void setTaskUrgent(){
             urgent = mView.findViewById(R.id.urgentIcon);
             urgent.setBackgroundResource(R.drawable.urgent_mark);
         }
+
     }
 }
